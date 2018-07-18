2d0b2d6cad73125272dad82405b9dc57861a97b6
Marko Thiele <marko.thiele@scale.eu>
2018-07-18 18:28:18 +0200

    created frist links beetween parts in assembly

---
+++ Cardboard_Car_-_freecad_assembly/start_freecad.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# this script assumes an alias o link `freecad` in the path
+# the freecad should use the Assembly3 workbench
+
+freecad `readlink -f ../Toyota_Yaris_-_freecad_assembly/create_assembly.py` `pwd`/cardboard_car___________________________PID0
