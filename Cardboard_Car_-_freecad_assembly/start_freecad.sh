7120c595a05558da7a2c259d5e6b97c19e62d55f
Marko Thiele <marko.thiele@scale.eu>
2018-07-18 21:49:12 +0200

    reverted some changes

--- Cardboard_Car_-_freecad_assembly/start_freecad.sh
+++ Cardboard_Car_-_freecad_assembly/start_freecad.sh
@@ -4,3 +4,11 @@
 # the freecad should use the Assembly3 workbench
 
 freecad `readlink -f ../Toyota_Yaris_-_freecad_assembly/create_assembly.py` `pwd`/cardboard_car___________________________PID0
+
+
+
+echo "deleting all fcstd1 files in: `pwd` ... since I do not want them in version control!"
+
+find -iname "*.fcstd1" | xargs -i rm -vrf {}
+find -iname "assembly.fcstd" | xargs -i rm -vrf {}
+
